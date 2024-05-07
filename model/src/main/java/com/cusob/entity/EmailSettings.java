4c955bd288b2c0ba958f5630979fc952e986adb9
Daybreak <daybreak@chtrak.com>
2024-05-07 13:42:26 +0800

    send email for register bug fix

--- model/src/main/java/com/cusob/entity/EmailSettings.java
+++ model/src/main/java/com/cusob/entity/EmailSettings.java
@@ -6,13 +6,21 @@ import lombok.Data;
 
 @TableName("email_settings")
 @Data
-public class EmailSettings {
+public class EmailSettings extends BaseEntity {
+
     @TableField("name_suffix")
     private String nameSuffix;
+
     @TableField("smtp_server")
     private String smtpServer;
+
     @TableField("imap_server")
     private String imapServer;
+
     @TableField("pop_server")
     private String popServer;
+
+    @TableField("start_tls")
+    private Integer startTls;
+
 }
