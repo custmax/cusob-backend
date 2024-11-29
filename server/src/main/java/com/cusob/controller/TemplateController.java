c2d15567933e3308ba6baba613a7aa046ac16524
马俊男 <2506774772@qq.com>
2024-11-29 11:53:16 +0800

    增加了根据userId向后端获取template的功能，修改了向前端返回数据的逻辑

--- server/src/main/java/com/cusob/controller/TemplateController.java
+++ server/src/main/java/com/cusob/controller/TemplateController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -50,7 +51,14 @@ public class TemplateController {
     @ApiOperation("get Folder List")
     @GetMapping("getFolderList")
     public Result getFolderList(){
-        List<String> folderList = templateService.getFolderList();
+        //List<String> folderList = templateService.getFolderList();
+        //todo 这里使用硬编码，应当设计一个template_category表
+        List<String> folderList = new ArrayList<>();
+        folderList.add("All");
+        folderList.add("Personal");
+        folderList.add("Welcome");
+        folderList.add("Seasons");
+        folderList.add("Deals & Offers");
         return Result.ok(folderList);
     }
 
